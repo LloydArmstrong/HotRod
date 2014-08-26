a75707494e658609099ae264db803670cb75f2b6
Stephan Buys <stephan.buys@panoptix.co.za>
2014-08-26 11:37:42 +0200

    Refactor bootstrap

--- hotrod.sh
+++ hotrod.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-if [[ "$HOTROD_URL" == "" ]]; then
-  EXTERNAL_IP=$(curl http://v4.ident.me 2>/dev/null)
-  RET=$?
-  echo "EXTERNAL_IP=$EXTERNAL_IP RET=$RET"
-  SHOSTED=$(ifconfig | grep ${EXTERNAL_IP})
-  FOUND="$?"
-
-  if [[ "$FOUND" != "0" && "$RET" == "0" ]]; then
-    echo "Not publicly hosted, auto bootstrap needs input."
-    echo "Please set environment variable HOTROD_URL"
-    exit 1
-  else
-    HOTROD_URL="https://${EXTERNAL_IP}"
-  fi
-fi
-
-if [[ $HOTROD_URL != *https* ]]; then
-  echo "Aborting, URL does not contain https://, HotRod was designed to work with https."
-  exit 1
-fi 
-
 set -e 
 
 docker build -t panoptix/hotrod .
@@ -28,14 +7,6 @@ docker build -t panoptix/hotrod .
 if [[ "$1" == "shell" ]]; then
   docker run --name hotrod -it --rm -v /var/run/docker.sock:/var/run/docker.sock panoptix/hotrod 
 else 
-  echo " " 
-  echo "Welcome."
-  echo " "
-  echo "About to bootstrap HotRod with : ${HOTROD_URL}"
-  echo "After initialisation (may take several minutes), access HotRod on ${HOTROD_URL}"
-  echo " "
-  echo "--- press CTRL-C to abort, Return to continue ---"
-  read
-  docker run --name hotrod -it --rm -v /var/run/docker.sock:/var/run/docker.sock panoptix/hotrod ./up.sh $EXTERNAL_IP
+  docker run --name hotrod -it --rm -v /var/run/docker.sock:/var/run/docker.sock panoptix/hotrod ./up.sh $HOTROD_URL
 fi
 
--- up.sh
+++ up.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 set -e
 
-hotrodurl="$1"
-export hotrodurl
+HOTROD_URL="$1"
+
+if [[ "$HOTROD_URL" == "" ]]; then
+  EXTERNAL_IP=$(curl http://v4.ident.me 2>/dev/null)
+  RET=$?
+  echo "EXTERNAL_IP=$EXTERNAL_IP RET=$RET"
+  SHOSTED=$(ifconfig | grep ${EXTERNAL_IP})
+  FOUND="$?"
+
+  if [[ "$FOUND" != "0" && "$RET" == "0" ]]; then
+    echo "Not publicly hosted, auto bootstrap needs input."
+    echo "Please set environment variable HOTROD_URL"
+    exit 1
+  else
+    HOTROD_URL="https://${EXTERNAL_IP}"
+  fi
+fi
+
+if [[ $HOTROD_URL != *https* ]]; then
+  echo "Aborting, URL does not contain https://, HotRod was designed to work with https."
+  exit 1
+fi 
+
+export HOTROD_URL
+
+echo " "
+echo "Welcome."
+echo " "
+echo "About to bootstrap HotRod with : ${HOTROD_URL}"
+echo "After initialisation (may take several minutes), access HotRod on ${HOTROD_URL}"
+echo " "
+echo "--- press CTRL-C to abort, Return to continue ---"
+read
 
 python /tmp/mini-templates/mini.py
 cd logger
-fig build
-fig up
+fig -p hotrodlogger build
+fig -p hotrodlogger up
 ~            
