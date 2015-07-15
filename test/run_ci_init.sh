b2102cd504328bc31fed0cc85cae9dd52ea02a5d
Stephan Buys <stephan.buys@panoptix.co.za>
2015-07-15 07:34:51 +0200

    Add Docker Hub login capability

--- test/run_ci_init.sh
+++ test/run_ci_init.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 export HOTROD_PROJNAME=Hotrod
 export HOTROD_HOSTNAME=$(docker-machine ip bkhotrod)
 export NOPROMPT=Yes
+
+echo "--- Log on to Docker Hub"
+
+[ -n "DOCKER_HUB_USERNAME" ] && {
+  docker login \
+  -e $DOCKER_HUB_EMAIL \
+  -p $DOCKER_HUB_PASSWORD \
+  -u $DOCKER_HUB_USERNAME
+}
+
+echo "+++ Run Docker Init"
 
 ./hotrod init
 
