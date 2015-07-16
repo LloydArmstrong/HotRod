c170f8e3f5854dc238b0fdab14f70187ddc20129
Stephan Buys <stephan.buys@panoptix.co.za>
2015-07-16 17:18:50 +0200

    Add ssh key adding script

--- test/add_ssh_keys.sh
+++ test/add_ssh_keys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ~/.ssh/ourkey| docker-machine ssh $BKHOTROD 'cat >> .ssh/authorized_keys && echo "Key copied"'
+cat ~/.ssh/ourkeys | docker-machine ssh $BKHOTROD 'cat >> .ssh/authorized_keys && echo "Key copied"'
