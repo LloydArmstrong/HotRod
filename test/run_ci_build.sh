1b542be8852733741fcc0149b5af6aeebd589760
Stephan Buys <stephan.buys@panoptix.co.za>
2015-10-27 14:46:49 +0200

    Fix syntax error in the build script, missing $

--- test/run_ci_build.sh
+++ test/run_ci_build.sh
@@ -3,7 +3,7 @@
 PROJNAME=$(echo $BUILDKITE_PROJECT_SLUG | cut -f2 -d'/')
 BKHOTROD=$PROJNAME-bkhotrod-$BUILDKITE_BUILD_NUMBER
 
-export DIGITALOCEAN_ACCESS_TOKEN=(vault read -field=token secret/CD/cloud/DigitalOceanCI)
+export DIGITALOCEAN_ACCESS_TOKEN=$(vault read -field=token secret/CD/cloud/DigitalOceanCI)
 export DIGITALOCEAN_REGION=fra1
 
 echo "--- check if $BKHOTROD machine exists"
