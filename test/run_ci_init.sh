20592c841548848496d18859d75ef21bb4c5b61a
Stephan Buys <stephan.buys@panoptix.co.za>
2015-11-12 12:28:05 +0200

    Remove tests from the install step

--- test/run_ci_init.sh
+++ test/run_ci_init.sh
@@ -5,9 +5,6 @@ set -eo pipefail
 PROJNAME=$(echo $BUILDKITE_PROJECT_SLUG | cut -f2 -d'/')
 BKHOTROD=$PROJNAME-bkhotrod-$BUILDKITE_BUILD_NUMBER
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-STARTTIME=$(date +%s)
-
 export HOTROD_PROJNAME=Hotrod
 export HOTROD_HOSTNAME=$(docker-machine ip $BKHOTROD)
 export NOPROMPT=Yes
@@ -30,19 +27,3 @@ echo "--- Log on to Docker Hub"
 echo "+++ Run HotRod Init"
 
 ./hotrod init
-
-set +e
-echo "+++ Run the goss tests"
-cd $DIR/local/tests
-TESTS=$(find . | grep '/goss_hotrod/' | grep -e '\.json$')
-for t in $TESTS; do
-  echo "+++ Running test: $t"
-  cat $t | docker-machine ssh $BKHOTROD 'goss validate'
-done
-
-TESTS=$(find . | grep '/goss_easyelk/' | grep -e '\.json$')
-for t in $TESTS; do
-  echo "+++ Running test: $t"
-  cat $t | docker-machine ssh $BKHOTROD 'goss validate'
-done
-
