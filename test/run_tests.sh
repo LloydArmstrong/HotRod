6ea4dc79d1ba91ca7a5dc518fcdd74954156111d
Stephan Buys <stephan.buys@panoptix.co.za>
2015-11-12 13:36:49 +0200

    The environment variable can be overwritten

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,10 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 STARTTIME=$(date +%s)
 set -e
 
-[ -n "$BKHOTROD" ]  || {
+if [ ! -n "$DOCKERMACHINE" ]; then
+  export PROJNAME=$(echo $BUILDKITE_PROJECT_SLUG | cut -f2 -d'/')
   export BKHOTROD=$PROJNAME-bkhotrod-$BUILDKITE_BUILD_NUMBER
-}
+else
+  export BKHOTROD=$DOCKERMACHINE
+fi
 
+[ -n "$BKHOTROD" ] || {
+  echo "A docker-machine must be defined..."
+  exit 1
+}
 echo "+++ Run the goss tests"
 
 cd $DIR/../local/tests
