f43602fce7ab9c0f1d9a52d50b68607ac23757cd
Stephan Buys <stephan.buys@panoptix.co.za>
2015-11-13 15:19:33 +0200

    Run 'all' tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -28,7 +28,7 @@ for t in $TESTS; do
   cat $t | docker-machine ssh $BKHOTROD 'goss validate'
 done
 
-TESTS=$(find . | grep '/goss_easyelk/' | grep -e '\.json$')
+TESTS=$(find . | grep -v '/goss_hotrod/' | grep -e '\.json$')
 for t in $TESTS; do
   echo 
   echo "+++ Running test: $t"
