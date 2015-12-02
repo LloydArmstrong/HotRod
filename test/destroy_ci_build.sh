6c5a245611959369741932af3d9d17bbc4c094a2
Stephan Buys <stephan.buys@panoptix.co.za>
2015-12-02 14:25:07 +0200

    Add a variable HOTROD_KEEPMACHINE, that if defined will cause the destroy docker step not to be run

--- test/destroy_ci_build.sh
+++ test/destroy_ci_build.sh
@@ -5,7 +5,10 @@ set -eo pipefail
 PROJNAME=$(echo $BUILDKITE_PROJECT_SLUG | cut -f2 -d'/')
 BKHOTROD=$PROJNAME-bkhotrod-$BUILDKITE_BUILD_NUMBER
 
-echo "+++ destroy $BKHOTROD machine"
-docker-machine rm -f $BKHOTROD
+[ -n "$HOTROD_KEEPMACHINE" ] || {
+  echo "+++ destroy $BKHOTROD machine"
+  docker-machine rm -f $BKHOTROD
+}
+
 
 echo "--- done"
